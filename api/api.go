0d455e40e46eb03806f821f7f06132ae1ad6f432
aretheregods <aretheregods@gmail.com>
2022-07-31 18:55:06 -0400

    Added get poems api func

--- api/api.go
+++ api/api.go
@@ -22,10 +22,15 @@ func InitializeAPI()  {
 	api := &APIRoutes{ poems }
 
 	app.Get("/poetry", api.GetAllPoems)
+	app.Get("/poetry/:id", api.GetPoem)
 
 	app.Listen(":4321")
 }
 
 func (routes *APIRoutes) GetAllPoems(c *fiber.Ctx) error {
+	return nil
+}
+
+func (routes *APIRoutes) GetPoem(c *fiber.Ctx) error {
 	return nil
 }
