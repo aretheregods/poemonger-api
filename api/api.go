8ac4db9d51039dc8a9754f83cfbac7313b6993b4
Naythaniel Williams <aretheregods@gmail.com>
2023-08-21 07:41:51 -0400

    Moved route funcs to separate file

--- api/api.go
+++ api/api.go
@@ -2,14 +2,9 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/mongo"
 
 	"poemonger/api/db"
 )
-
-type APIRoutes struct {
-	DB *mongo.Database
-}
 
 func InitializeAPI() {
 	app := fiber.New()
@@ -28,51 +23,4 @@ func InitializeAPI() {
 	})
 
 	app.Listen(":4321")
-}
-
-func (routes *APIRoutes) GetAllPoems(c *fiber.Ctx) error {
-	return c.SendString("These are a bunch of poems")
-}
-
-func (routes *APIRoutes) GetPoem(c *fiber.Ctx) error {
-	return c.SendString("This is a poem")
-}
-
-func (routes *APIRoutes) PostPoem(c *fiber.Ctx) error {
-	p := new(db.Poem)
-
-	if err := c.BodyParser(p); err != nil {
-		return err
-	}
-
-	coll := routes.DB.Collection("poetry")
-	_, err := coll.InsertOne(c.Context(), p)
-	if err != nil {
-		return err
-	}
-
-	return c.SendStatus(200)
-}
-
-func (routes *APIRoutes) GetWorks(c *fiber.Ctx) error {
-	return c.SendString("These are collections of poems")
-}
-
-func (routes *APIRoutes) GetWork(c *fiber.Ctx) error {
-	return c.SendString("This is a poetry collection")
-}
-
-func (routes *APIRoutes) PostWork(c *fiber.Ctx) error {
-	w := new(db.Work)
-
-	if err := c.BodyParser(w); err != nil {
-		return err
-	}
-
-	coll := routes.DB.Collection("works")
-	if _, err := coll.InsertOne(c.Context(), w); err != nil {
-		return err
-	}
-
-	return c.SendStatus(200)
 }
---
+++ api/routes.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"poemonger/api/db"
+)
+
+type APIRoutes struct {
+	DB *mongo.Database
+}
+
+func (routes *APIRoutes) GetAllPoems(c *fiber.Ctx) error {
+	return c.SendString("These are a bunch of poems")
+}
+
+func (routes *APIRoutes) GetPoem(c *fiber.Ctx) error {
+	return c.SendString("This is a poem")
+}
+
+func (routes *APIRoutes) PostPoem(c *fiber.Ctx) error {
+	p := new(db.Poem)
+
+	if err := c.BodyParser(p); err != nil {
+		SendBasicError(c, err, 400)
+	}
+
+	coll := routes.DB.Collection("poetry")
+	res, err := coll.InsertOne(c.Context(), p)
+	if err != nil {
+		return SendBasicError(c, err, 400)
+	}
+
+	return c.Status(201).JSON(fiber.Map{
+		"id": fmt.Sprint(res.InsertedID),
+		"link": fmt.Sprintf("/poetry/%v", res.InsertedID),
+	})
+}
+
+func (routes *APIRoutes) GetWorks(c *fiber.Ctx) error {
+	return c.SendString("These are collections of poems")
+}
+
+func (routes *APIRoutes) GetWork(c *fiber.Ctx) error {
+	return c.SendString("This is a poetry collection")
+}
+
+func (routes *APIRoutes) PostWork(c *fiber.Ctx) error {
+	w := new(db.Work)
+
+	if err := c.BodyParser(w); err != nil {
+		return SendBasicError(c, err, 400)
+	}
+
+	coll := routes.DB.Collection("works")
+	res, err := coll.InsertOne(c.Context(), w)
+	if err != nil {
+		return SendBasicError(c, err, 400)
+	}
+
+	return c.Status(201).JSON(fiber.Map{
+		"id": fmt.Sprint(res.InsertedID),
+		"link": fmt.Sprintf("/work/%v", res.InsertedID),
+	})
+}
